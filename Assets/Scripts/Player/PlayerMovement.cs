89ee8753dff9aaa7d5140f5866437f6e10954db4
Anthony Mojica <Mojica@fullsail.edu>
2015-04-17 15:50:23 -0400

    Comments added to Player movement Health Script work done(incomplete)

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -12,7 +12,7 @@ public class PlayerMovement : MonoBehaviour {
     void Start()
     {
         controller = gameObject.GetComponent<CharacterController>();
-        speed = .05f;
+        speed = 1;
     }
 
 	void Update ()
@@ -22,23 +22,31 @@ public class PlayerMovement : MonoBehaviour {
    
     void Move()
     {
+
+        //Check for Left Stick Axis to 
+        //see if it is surpressed fully '
+        //for more speed
         if (Input.GetAxis("Horizontal") > .8f || Input.GetAxis("Vertical") > .8f || Input.GetAxis("Vertical") < -.8f || Input.GetAxis("Horizontal") < -.8f)
         {
-            speed = 4.5f;
+            speed = 3.1f;
             // print("Speedup");
         }
         else
-            speed = 2.5f;
+            speed = 1.6f;
 
+        //Check Left Joysticks for Movement
         MoveDirect.x = Input.GetAxis("Horizontal");
         MoveDirect.y = Input.GetAxis("Vertical");
 
+        //Normalize the directional vector
+        //Factor in speed and time
         MoveDirect.Normalize();
         MoveDirect *= speed * Time.deltaTime;
-        //Move();
 
+        //Actually Move the player
         controller.Move(MoveDirect);
 
+        //Rotate the player to where they are moving
         if (MoveDirect != Vector2.zero)
         {
             float angle = Mathf.Atan2(MoveDirect.y, MoveDirect.x) * Mathf.Rad2Deg;
@@ -55,22 +63,39 @@ public class PlayerMovement : MonoBehaviour {
 
     void Rotate()
     {
+        //Check Right sticks for Rotation
         MoveDirect.x = Input.GetAxis("RStickHorizontal");
         MoveDirect.y = Input.GetAxis("RStickVertical");
         MoveDirect.Normalize();
 
+        //If Right sticks have input
+        //override the Move() function rotation
+        //with this one for aiming
+        if (MoveDirect != Vector2.zero)
+        {
+            float angle = Mathf.Atan2(MoveDirect.y, -MoveDirect.x) * Mathf.Rad2Deg;
+            angle += 90;
+            controller.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+        if (MoveDirect != Vector2.zero)
+        {
+            float angle = Mathf.Atan2(MoveDirect.y, -MoveDirect.x) * Mathf.Rad2Deg;
+            angle += 90;
+            controller.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+    void MouseRotate()
+    {
 
-        if (MoveDirect != Vector2.zero)
-        {
-            float angle = Mathf.Atan2(MoveDirect.y, -MoveDirect.x) * Mathf.Rad2Deg;
-            angle += 90;
-            controller.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        }
-        if (MoveDirect != Vector2.zero)
-        {
-            float angle = Mathf.Atan2(MoveDirect.y, -MoveDirect.x) * Mathf.Rad2Deg;
-            angle += 90;
-            controller.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        }
+
+
+
+
+
+
+
+
+
+
     }
 }
