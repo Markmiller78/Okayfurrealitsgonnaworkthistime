f2f0a243ec306ad7cb89bd3001d94130df999201
Michael <Billiam94@gmail.com>
2015-04-17 16:45:09 -0400

    Worked on InputManager.cs.

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -5,6 +5,7 @@ public class InputManager : MonoBehaviour
 {
     GameObject player;
     Vector3 mousePrevPos;
+    bool isControllerConnected = false;
 
     void Start()
     {
@@ -13,6 +14,10 @@ public class InputManager : MonoBehaviour
 
     void Update()
     {
+        if (Input.GetJoystickNames().Length != 0)
+        {
+            isControllerConnected = true;
+        }
         if (Input.GetAxis("Horizontal") != 0.0f || Input.GetAxis("Vertical") != 0.0f)
         {
             player.SendMessage("Move");
@@ -21,10 +26,14 @@ public class InputManager : MonoBehaviour
         {
             player.SendMessage("Rotate");
         }
-        else if (mousePrevPos != Input.mousePosition)
+        else if (!isControllerConnected && mousePrevPos != Input.mousePosition)
         {
             player.SendMessage("MouseRotate");
         }
         mousePrevPos = Input.mousePosition;
+        if (Input.GetButtonDown("Dash"))
+        {
+            player.SendMessage("Dash", SendMessageOptions.DontRequireReceiver);
+        }
     }
 }
