52d831a7658dd791aec4b8d365a3c08f18436eb9
Anthony Mojica <Mojica@fullsail.edu>
2015-04-18 19:55:32 -0400

    Worked on Main Menu

---
+++ Assets/light_pulse.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class light_pulse : MonoBehaviour {
+
+    float Intensity;
+    float Range;
+	// Use this for initialization
+	void Start () {
+	//Intensity = gameObject.GetComponent<Intensity
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
