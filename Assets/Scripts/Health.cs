19db4300372ecff6fe409f62a64be26fcadb2b58
Anthony Mojica <Mojica@fullsail.edu>
2015-04-18 18:33:58 -0400

    Health bar implemented Health script completed for current state - (no support for enemy health bars)

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -6,15 +6,27 @@ public class Health : MonoBehaviour
 
     public float maxHP;
     public float currentHP;
+    public float healthPercent;
+    GameObject healthBar;
     void Start()
     {
         maxHP = 100;
+        healthBar = GameObject.FindGameObjectWithTag("HealthBar");
+        healthPercent = currentHP / maxHP * 100;
+        
+        //LoseHealth(20); //Used for testing
     }
     void GainHealth(float Amount)
     {
         currentHP += Amount;
         if (currentHP > maxHP)
             currentHP = maxHP;
+
+        if(this.tag == "Player")
+        {
+            healthPercent = currentHP / maxHP;
+            healthBar.transform.localScale = new Vector3(1, healthPercent, 1);
+        }
     }
 
     void LoseHealth(float Amount)
@@ -22,6 +34,12 @@ public class Health : MonoBehaviour
         currentHP -= Amount;
         if (currentHP <= 0)
             Die();
+
+        if (this.tag == "Player")
+        {
+            healthPercent = currentHP / maxHP;
+            healthBar.transform.localScale = new Vector3(1, healthPercent, 1);
+        }
     }
 
     void Die()
