37573d42c591e53a957f9b886f5f5612dbfc06dd
Mark Miller <guitar8155@gmail.com>
2015-04-18 17:48:43 -0400

    Created the light pick up and gave the player the ability to dash, starting with the trailblazer boots. I have it set up for an enum in the player equipment script, so trailblazer boots changes the variable to 0 and then the dash script reads in what the integer value of the players equippedBoot is and acts accordingly

--- Assets/Scripts/Player/PlayerEquipment.cs
+++ Assets/Scripts/Player/PlayerEquipment.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum boots { Trailblazer = 0, Whirlwind, Charge, Decoy, Blink };
+
+
 public class PlayerEquipment : MonoBehaviour {
 
-    public GameObject equippedAccessory;
-    public GameObject equippedBoots;
-    public GameObject equippedEmber;
+    public int equippedAccessory;
+    public int equippedBoots;
+    public int equippedEmber;
+
 
 
 }
