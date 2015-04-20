0797c7d10dcb557fa6ce7f1c18f3c091fe00be95
Anthony Mojica <Mojica@fullsail.edu>
2015-04-20 09:17:59 -0400

    Main Menu Started

--- Assets/light_pulse.cs
+++ Assets/light_pulse.cs
@@ -3,15 +3,27 @@ using System.Collections;
 
 public class light_pulse : MonoBehaviour {
 
-    float Intensity;
-    float Range;
+    public Light lt;
+    public float lIntensity;
+    public float lRange;
+    public bool grow;
 	// Use this for initialization
 	void Start () {
-	//Intensity = gameObject.GetComponent<Intensity
+        
+        lt = GetComponent<Light>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (lt.intensity < 2)
+            grow = true;
+
+        if (lt.intensity >= 4)
+            grow = false;
+
+        if (grow)
+            lt.intensity += Time.deltaTime;
+        else
+            lt.intensity -= Time.deltaTime;
 	}
 }
