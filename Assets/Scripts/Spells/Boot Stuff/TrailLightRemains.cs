1eae370a8e528dd8ed0c87bd34a779560cbe4e60
Mark Miller <guitar8155@gmail.com>
2015-04-22 19:55:54 -0400

    Created light pick ups, trailoflight ready for check off

---
+++ Assets/Scripts/Spells/Boot Stuff/TrailLightRemains.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrailLightRemains : MonoBehaviour {
+
+    PlayerLight heroLight;
+    bool active;
+
+    void Start()
+    {
+        heroLight = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerLight>();
+        active = true;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (active)
+        {
+            if (other.tag == "Player")
+            {
+                //The dash currently spawns 11 of these as of 4/22/15
+                heroLight.GainLight(1);
+                active = false;
+            }
+        }
+
+    }
+}
--- Assets/Scripts/Spells/Boot Stuff/TrailProjectile.cs
+++ Assets/Scripts/Spells/Boot Stuff/TrailProjectile.cs
@@ -10,6 +10,8 @@ public class TrailProjectile : MonoBehaviour
     ParticleSystem particles;
     Light particleLight;
 
+    public GameObject TrailLightRemains;
+
     void Start()
     {
         //Orient the thing to look correct
@@ -22,7 +24,7 @@ public class TrailProjectile : MonoBehaviour
         timeAlive += Time.deltaTime;
 
         //Turn off the particles early to make deletion look more smooth
-        if (timeAlive > (MaxTimeActive - 0.7))
+        if (timeAlive >= (MaxTimeActive - 0.7))
         {
             particles.emissionRate = 0;
             particleLight.range -= Time.deltaTime;
@@ -30,7 +32,22 @@ public class TrailProjectile : MonoBehaviour
 
         if (timeAlive > MaxTimeActive)
         {
+            //Spawn the trail light remains and destory the gameobject
+            Instantiate(TrailLightRemains, transform.position, new Quaternion(0, 0, 0, 0));
             Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //If the trail is still in its damage phase
+        if (timeAlive < (MaxTimeActive - 0.7))
+        {
+            if (other.tag == "Enemy")
+            {
+                other.GetComponent<Health>().LoseHealth(3);
+                timeAlive = (MaxTimeActive - 0.7f);
+            }
         }
     }
 }
