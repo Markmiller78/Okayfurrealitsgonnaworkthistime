66c1872c5c27590bc45112d6605b108ffb7e1bee
Mark <guitar8155@gmail.com>
2015-04-23 10:27:28 -0400

    Made trail of light remains pickupable

--- Assets/Scripts/Spells/Boot Stuff/TrailLightRemains.cs
+++ Assets/Scripts/Spells/Boot Stuff/TrailLightRemains.cs
@@ -1,15 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TrailLightRemains : MonoBehaviour {
+public class TrailLightRemains : MonoBehaviour
+{
 
     PlayerLight heroLight;
     bool active;
 
+    float deathTimer;
+    ParticleSystem particles;
+    Light particleLight;
+    SpriteRenderer sprite;
+    bool once;
+
+    public GameObject trailParticles;
+
     void Start()
     {
         heroLight = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerLight>();
+        particles = gameObject.GetComponentInChildren<ParticleSystem>();
+        particleLight = gameObject.GetComponent<Light>();
+        sprite = gameObject.GetComponent<SpriteRenderer>();
         active = true;
+        deathTimer = 0;
+        once = true;
     }
 
     void OnTriggerEnter(Collider other)
@@ -24,5 +38,29 @@ public class TrailLightRemains : MonoBehaviour {
             }
         }
 
+    }
+
+    void Update()
+    {
+        if (!active)
+        {
+            deathTimer += Time.deltaTime;
+
+            particles.emissionRate = 0;
+            sprite.enabled = false;
+
+            if (once)
+            {
+                Instantiate(trailParticles, transform.position, new Quaternion(0, 0, 0, 0));
+                once = false;
+            }
+
+            particleLight.range -= Time.deltaTime;
+
+            if (deathTimer > 0.7)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
