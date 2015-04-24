f17396876349aa19d17d0ca8c442622cb2500331
Michael <Billiam94@gmail.com>
2015-04-24 10:42:19 -0400

    Fixed a bug in Knockback.cs that was causing enemies to get stuck in corners in certain circumstances. Did a tiny bit of work on Dethros before I found said bug.

--- Assets/Scripts/Enemies/Bosses/BossAIDethros.cs
+++ Assets/Scripts/Enemies/Bosses/BossAIDethros.cs
@@ -3,13 +3,49 @@ using System.Collections;
 
 public class BossAIDethros : MonoBehaviour
 {
+    GameObject player;
+    float distanceToPlayer;
+    public float meleeRange;
+    public float spellMinRange;
+    public float spellMaxRange;
+    public float moveSpeed;
+    CharacterController controller;
 
     void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+        MoveAwayFromPlayer();
+    }
+
+    void MoveTowardPlayer()
+    {
+        Vector2 moveTo = (player.transform.position - transform.position).normalized;
+        controller.Move(moveTo * Time.deltaTime * moveSpeed);
+    }
+
+    void MoveAwayFromPlayer()
+    {
+        Vector2 moveTo = (player.transform.position - transform.position).normalized;
+        controller.Move(moveTo * Time.deltaTime * -moveSpeed);
+    }
+
+    void MeleeAttack()
     {
 
     }
 
-    void Update()
+    void SpellAttack()
+    {
+
+    }
+
+    void Summon()
     {
 
     }
