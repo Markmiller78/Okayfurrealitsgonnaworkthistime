bfb6eb1ea2eb59f20adaccc61e53ac91796eb480
Mark Miller <guitar8155@gmail.com>
2015-04-24 20:39:47 -0400

    Fire ember and frost ember fully functioning

---
+++ Assets/Scripts/Spells/Ember/DebuffFire.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebuffFire : MonoBehaviour
+{
+
+    public float dps;
+    float timer;
+    public GameObject target;
+
+    Health targetHealth;
+    ParticleSystem particles;
+    Light light;
+    void Start()
+    {
+        timer = 0;
+        targetHealth = target.GetComponent<Health>();
+        particles = gameObject.GetComponent<ParticleSystem>();
+        light = gameObject.GetComponent<Light>();
+    }
+
+    void Update()
+    {
+
+        if (target == null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            transform.position = target.transform.position + new Vector3(0, 0, -0.5f);
+            timer += Time.deltaTime;
+
+
+
+            if (timer < 3.0f)
+            {
+                targetHealth.LoseHealth(dps * Time.deltaTime);
+            }
+            else
+            {
+                particles.emissionRate = 0;
+                light.range -= Time.deltaTime;
+            }
+
+            if (timer > 4.0f)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+}
--- Assets/Scripts/Spells/Orb of Light/OrbExplosion.cs
+++ Assets/Scripts/Spells/Orb of Light/OrbExplosion.cs
@@ -4,12 +4,29 @@ using System.Collections;
 public class OrbExplosion : MonoBehaviour
 {
     Light theLight;
+    PlayerEquipment heroEquipment;
     float timeAlive;
+
+    public GameObject debuff;
+
+    float maxLife;
+
     // Use this for initialization
     void Start()
     {
         timeAlive = 0;
         theLight = gameObject.GetComponent<Light>();
+        heroEquipment = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerEquipment>();
+
+        if (heroEquipment.equippedEmber == ember.Ice)
+        {
+            maxLife = 1.1f;
+        }
+        else
+        {
+            maxLife = 0.6f;
+        }
+
     }
 
     // Update is called once per frame
@@ -17,7 +34,7 @@ public class OrbExplosion : MonoBehaviour
     {
         timeAlive += Time.deltaTime;
         theLight.range -= Time.deltaTime * 4;
-        if (timeAlive >= 1.0f)
+        if (timeAlive >= maxLife)
         {
             Destroy(gameObject);
         }
@@ -27,7 +44,22 @@ public class OrbExplosion : MonoBehaviour
     {
         if (other.tag == "Enemy")
         {
-            other.GetComponent<Health>().LoseHealth(5);            
+            if (heroEquipment.equippedEmber == ember.None)
+            {
+                other.GetComponent<Health>().LoseHealth(5);                            
+            }
+            else if (heroEquipment.equippedEmber == ember.Fire)
+            {
+                other.GetComponent<Health>().LoseHealth(5);
+                GameObject tempObj = (GameObject)Instantiate(debuff, other.transform.position, other.transform.rotation);
+                tempObj.GetComponent<DebuffFire>().target = other.gameObject;
+            }
+            else if (heroEquipment.equippedEmber == ember.Ice)
+            {
+                other.GetComponent<Health>().LoseHealth(5);
+                GameObject tempObj = (GameObject)Instantiate(debuff, other.transform.position, other.transform.rotation);
+                tempObj.GetComponent<DebuffFrost>().target = other.gameObject;
+            }
         }
     }
 }
