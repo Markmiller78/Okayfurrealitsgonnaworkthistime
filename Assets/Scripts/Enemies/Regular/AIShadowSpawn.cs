5c6a7ebe703e0da47478f77bcdfd36df34635550
Anthony Mojica <Mojica@fullsail.edu>
2015-04-26 18:07:45 -0400

    ShadowSpawn AI work

--- Assets/Scripts/Enemies/Regular/AIShadowSpawn.cs
+++ Assets/Scripts/Enemies/Regular/AIShadowSpawn.cs
@@ -6,64 +6,216 @@ public class AIShadowSpawn : MonoBehaviour
 
     GameObject player;
     Health playerHealth;
+    PlayerLight playerLight;
     CharacterController controller;
 
-    public float damage;
-    public float damageRate;
-    public float moveSpeed;
-    float distanceToPlayer;
+    Vector2 Moveto;
+    Vector3 WayPoint;
+    public float speed = 1;
+    float DistancetoPlayer;
+    float timer = 1;
+    int timerCount = 1;
+    bool newWayPoint = true;
+    float minSpeed = 1;
+    float maxSpeed = 5;
 
-    Vector2 fleeDirection;
-
-    bool fleeing;
-    float fleeTimer;
-    float fleeDuration;
-    float fleeCooldown;
+    GameObject PrimaryThreat;
+    GameObject SecondaryThreat;
+    public enum State { Idle = 0, Evade, SuperEvade, Enrage }
+    State CurrentState = State.Idle;
 
     void Start()
     {
+        Random.seed = 42;
         player = GameObject.FindGameObjectWithTag("Player");
+        playerLight = player.GetComponent<PlayerLight>();
+        Moveto = new Vector3(0, 0);
         controller = GetComponent<CharacterController>();
-        fleeing = false;
-        fleeTimer = 0;
-        fleeDuration = 0.5f;
+        speed = 1;
+        timerCount = 1;
+        minSpeed = 1;
+        maxSpeed = 1;
+        newWayPoint = true;
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+        timer -= Time.deltaTime;
 
-        if (fleeing)
+        //calculate distance to player.
+        if (timer < 0)
         {
-            Flee();
+            float num1 = (this.transform.position.x - player.transform.position.x);
+            num1 *= num1;
+            float num2 = (this.transform.position.y - player.transform.position.y);
+            num2 *= num2;
+            DistancetoPlayer = Mathf.Sqrt(num1 + num2);
+            timer = 1;
+            print(DistancetoPlayer);
+            print(CurrentState);
+            timerCount += 1;
         }
-        else
+
+        //"Animated" Movement
+        if (newWayPoint)
         {
-            //If the player is close, move away
-            if (distanceToPlayer <= 10)
+            speed = maxSpeed;
+            newWayPoint = false;
+        }
+        if(speed > 0)
+            speed -= 2* Time.deltaTime;
+
+        //Locate Primary and 2ndary Threat
+        PrimaryThreat = findNearestWithTag(transform.position, "Player");
+        
+
+
+
+
+        //Determine Which behavior to run
+        if (playerLight.currentLight < 10)
+        {
+            CurrentState = State.Enrage;
+        }
+        if (DistancetoPlayer > 7)
+            CurrentState = State.Idle;
+        else if (DistancetoPlayer < 7 && DistancetoPlayer > 3)
+        {
+            CurrentState = State.Evade;
+        }
+        else if (DistancetoPlayer < 3)
+        {
+            CurrentState = State.SuperEvade;
+        }
+
+
+
+
+        //Call the Current behavior
+        switch (CurrentState)
+        {
+            case State.Idle:
+                {
+                    Idle();
+                    break;
+                }
+            case State.Evade:
+                {
+                    Evade();
+                    break;
+                }
+            case State.SuperEvade:
+                {
+                    SuperEvade();
+                    break;
+                }
+            case State.Enrage:
+                {
+                    Enrage();
+                    break;
+                }
+        }
+        Moveto.Normalize();
+        Moveto *= speed * Time.deltaTime;
+        if(speed > 1)
+        controller.Move(Moveto);
+    }
+
+    void Idle()
+    {
+        minSpeed = 0;
+        maxSpeed = 3;
+
+        //Change Idle Waypoint every 2 seconds.
+        if (timerCount > 2)
+        {
+            timerCount = 0;
+            float newX = Random.Range(-2f, 2f);
+            float newY = Random.Range(-2f, 2f);
+            WayPoint = new Vector3(transform.position.x + newX, transform.position.y + newY, -1.4f);
+
+            Moveto = transform.position - WayPoint;
+            newWayPoint = true;
+        }
+    }
+
+    void Evade()
+    {
+        
+        if(timerCount > 1 )
+        {
+            timerCount = 0;
+
+            WayPoint = PrimaryThreat.transform.position - transform.position;
+            Moveto = WayPoint;
+            newWayPoint = true;
+        }
+
+    }
+
+    void SuperEvade()
+    {
+        minSpeed = 1;
+        maxSpeed = 4;
+        if (timerCount > 1)
+        {
+            timerCount = 0;
+
+            WayPoint = PrimaryThreat.transform.position - transform.position;
+            Moveto = WayPoint;
+            newWayPoint = true;
+            Jump();
+        }
+
+    }
+
+    void Enrage()
+    {
+        if (timerCount > 1)
+        {
+            timerCount = 0;
+
+            WayPoint = transform.position - PrimaryThreat.transform.position;
+            Moveto = WayPoint;
+            newWayPoint = true;
+            
+        }
+        
+
+    }
+
+    void Jump()
+    {
+
+        speed = 7;
+    }
+
+    public static GameObject findNearestWithTag(Vector3 fromPosition, string tag)
+    {
+        GameObject[] gos = GameObject.FindGameObjectsWithTag(tag);
+
+        if (gos.Length == 1)
+            return gos[0];
+
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = fromPosition;
+
+        foreach (GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
             {
-                MoveAway();
+                closest = go;
+                distance = curDistance;
             }
         }
 
+        return closest;
     }
 
-    void MoveAway()
-    {
-        Vector2 moveTo = (transform.position - player.transform.position).normalized;
-        controller.Move(moveTo * Time.deltaTime * moveSpeed);
-        transform.LookAt(transform.up);
-    }
-
-    void Flee()
-    {
-        controller.Move(fleeDirection * Time.deltaTime * 10);
-
-        fleeTimer -= Time.deltaTime;
-        if (fleeTimer <= 0)
-        {
-            fleeing = false;
-        }
-    }
 }
---
+++ Assets/Scripts/ParticleAttract.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleAttract : MonoBehaviour {
+
+        public ParticleSystem particleSystem;
+
+ 
+	// Use this for initialization
+	void Start () 
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+       
+    
+    
+    }
+
+     private void AddForceOnParticles()
+     {
+         
+         ParticleSystem.Particle[] _particles = new ParticleSystem.Particle[particleSystem.particleCount];
+         this.particleSystem.GetParticles(_particles);
+         
+         for (int i = 0; i < _particles.Length; i++){
+             // Here you can do whatever you want with each particle by using _particles[i] 
+         }
+ 
+         this.particleSystem.SetParticles(_particles, 15);
+     }
+}
