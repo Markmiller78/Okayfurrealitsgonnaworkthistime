180fd00de7fb29848241b508297dba5a2bd6cd1a
truesora <jvalerohernandez@fullsail.edu>
2015-04-27 11:30:47 -0400

    updating tooltips

---
+++ Assets/Scripts/Player/ToolTips.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ToolTips : MonoBehaviour {
+
+	public GameObject[] objects;
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		objects = GameObject.FindObjectsOfType <GameObject>();
+		foreach (GameObject obj in objects)
+		{
+			if((transform.position-obj.transform.position).magnitude<1.0f)
+			obj.SendMessage("DisplayTooltip", SendMessageOptions.DontRequireReceiver);
+			else
+				obj.SendMessage("DoNotDisplayTooltip", SendMessageOptions.DontRequireReceiver);
+		}
+	
+	}
+}
