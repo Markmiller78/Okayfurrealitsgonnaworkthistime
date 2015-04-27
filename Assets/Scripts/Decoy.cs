63753f5c6a4167253bcbb2aa7a98e418897c2e46
truesora <jvalerohernandez@fullsail.edu>
2015-04-27 14:05:04 -0400

    More decoy!

---
+++ Assets/Scripts/Decoy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Decoy : MonoBehaviour {
+
+
+	float timer=5.0f;
+	// Use this for initialization
+	void Start ()  {
+		GameObject[] allObjects;
+		allObjects = GameObject.FindObjectsOfType<GameObject>();
+		foreach (GameObject obj in allObjects)
+		{
+			obj.SendMessage("Decoy", SendMessageOptions.DontRequireReceiver);
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
