908cbb5104adf36ee7a704716be48fa21e17cfae
Mark <guitar8155@gmail.com>
2015-04-27 14:35:55 -0400

    Staff particles implemented

---
+++ Assets/WindStaffParticles.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindStaffParticles : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //WORLD
+        transform.Rotate(new Vector3(0, 600 * Time.deltaTime, 0));
+	}
+}
