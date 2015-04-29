cde7ed80a92357b963baa1cd6cccb10d14166c2e
Mark Miller <guitar8155@gmail.com>
2015-04-28 22:50:15 -0400

    Options menu working

---
+++ Assets/MenuMusic.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuMusic : MonoBehaviour {
+
+    int musicMultiplier;
+
+    AudioSource soundPlayer;
+
+	// Use this for initialization
+	void Start () {
+        soundPlayer = gameObject.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        musicMultiplier = GameObject.Find("TheOptions").GetComponent<Options>().musicVolume;
+        soundPlayer.volume = musicMultiplier * 0.01f;
+	}
+}
