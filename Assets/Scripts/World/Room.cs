041317824e3cdc795e64e8959465800eeee16a13
Michael <Billiam94@gmail.com>
2015-05-05 15:53:59 -0400

    Started the waypoint system and did some refactoring.

--- Assets/Scripts/World/Room.cs
+++ Assets/Scripts/World/Room.cs
@@ -30,7 +30,7 @@ public class Room : MonoBehaviour
     public bool beenThere;
     public int numEnemies = 0;
     public bool comingFromEntrance = true;
-
+    public Vector2[] waypointLocations;
 
     public void setUsed()
     {
@@ -40,89 +40,5 @@ public class Room : MonoBehaviour
             comingFromEntrance = true;
             enemySpawnPointUsed[i] = false;
         }
-    }
-
-    public void SpawnEnemies(int RoomCode)
-    {
-        //ROOM CODE WORKS AS A TIER THEN ROOM NUMBER
-        // 11 INDICATES TIER 1 ROOM 1
-        // 110 INDICATES TIER 1 ROOM 10
-        // 0 AFTER TIER INDICATES A MAZE
-        // 101 INDICATES TIER 1 MAZE 1
-        if(RoomCode == 11)
-        {
-
-        }
-        if (RoomCode == 12)
-        {
-            Instantiate(enemiesThatCanSpawn[Random.Range(0, 3)], new Vector3(4.89f, -1.54f, -1), new Quaternion(0,0,0,0));
-
-
-
-
-        }
-        if (RoomCode == 13)
-        {
-
-        }
-        if (RoomCode == 14)
-        {
-
-        }
-        if (RoomCode == 15)
-        {
-
-        }
-        if (RoomCode == 16)
-        {
-
-        }
-        if (RoomCode == 17)
-        {
-
-        }
-        if (RoomCode == 18)
-        {
-
-        }
-        if (RoomCode == 19)
-        {
-
-        }
-        if (RoomCode == 110)
-        {
-
-        }
-        if (RoomCode == 101)
-        {
-
-        }
-        if (RoomCode == 102)
-        {
-
-        }
-        if (RoomCode == 103)
-        {
-
-        }
-        if (RoomCode == 104)
-        {
-
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 }
