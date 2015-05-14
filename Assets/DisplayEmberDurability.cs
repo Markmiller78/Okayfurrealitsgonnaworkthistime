e26d9d49db99b56cb85aa51e65ce7301b758036c
Mark <guitar8155@gmail.com>
2015-05-14 13:48:29 -0400

    ui stuff

---
+++ Assets/DisplayEmberDurability.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DisplayEmberDurability : MonoBehaviour {
+
+    float timer;
+    public Text theText;
+
+    public int count;
+
+    PlayerEquipment heroEquip;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        heroEquip = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerEquipment>();
+
+        timer = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            timer = 1;
+            count = heroEquip.emberDurability;
+            theText.text = count.ToString();
+        }
+    }
+}
