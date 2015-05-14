0c61b9b3cf46242c36056a7cb47f556e1fc320f9
Mark <guitar8155@gmail.com>
2015-05-14 17:45:39 -0400

    enemy visible in dark

---
+++ Assets/EnemyLight.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyLight : MonoBehaviour {
+
+    Light thelight;
+
+    int phase;
+
+	// Use this for initialization
+	void Start () {
+        thelight = gameObject.GetComponent<Light>();
+        phase = 0;
+        thelight.range = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (phase == 0)
+        {
+            thelight.range = thelight.range + Time.deltaTime;
+            if (thelight.range >= 3f)
+            {
+                phase = 1;
+            }
+        }
+        else
+        {
+            thelight.range = thelight.range - Time.deltaTime;
+            if (thelight.range <= 1.5f)
+            {
+                phase = 0;
+            }
+        }
+	}
+}
