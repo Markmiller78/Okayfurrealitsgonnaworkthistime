f35f234380e0f4e9c326da4aa20ab7c90a972546
Michael <Billiam94@gmail.com>
2015-05-20 19:52:39 -0400

    Map Doors.

--- Assets/MapAndStatsMenu.cs
+++ Assets/MapAndStatsMenu.cs
@@ -151,30 +151,47 @@ public class MapAndStatsMenu : MonoBehaviour
                         generator.finalRoomInfoArray[i].width * 5,
                         generator.finalRoomInfoArray[i].height * 5),
                         "", i == generator.currentRoom ? crStyle : style);
+                    switch (generator.finalRoomInfoArray[i].entranceDir)
+                    {
+                        case 0:
+                            GUI.Label(new Rect(offset.x + (generator.finalRoomInfoArray[i].width / 2) * 5, Screen.height - (offset.y + 5), 5, 5),
+                                "", doorStyle);
+                            break;
+                        case 1:
+                            GUI.Label(new Rect(offset.x, Screen.height - (offset.y + (generator.finalRoomInfoArray[i].height / 2) * 5), 5, 5),
+                                "", doorStyle);
+                            break;
+                        case 2:
+                            GUI.Label(new Rect(offset.x + (generator.finalRoomInfoArray[i].width / 2) * 5, Screen.height - (offset.y + generator.finalRoomInfoArray[i].height * 5), 5, 5),
+                                "", doorStyle);
+                            break;
+                        case 3:
+                            GUI.Label(new Rect(offset.x - 5 + generator.finalRoomInfoArray[i].width * 5, Screen.height - (offset.y + (generator.finalRoomInfoArray[i].height / 2) * 5), 5, 5),
+                                "", doorStyle);
+                            break;
+                        default:
+                            break;
+                    }
                     switch (generator.finalRoomInfoArray[i].exitDir)
                     {
                         case 0:
-                            GUI.Label(new Rect(offset.x, Screen.height - (offset.y), 5, 5),
+                            GUI.Label(new Rect(offset.x + (generator.finalRoomInfoArray[i].width / 2) * 5, Screen.height - (offset.y + 5), 5, 5),
                                 "", doorStyle);
-
                             offset.y -= 25 + generator.finalRoomInfoArray[i].height * 6;
                             break;
                         case 1:
-                            GUI.Label(new Rect(offset.x, Screen.height - (offset.y), 5, 5),
+                            GUI.Label(new Rect(offset.x, Screen.height - (offset.y + (generator.finalRoomInfoArray[i].height / 2) * 5), 5, 5),
                                 "", doorStyle);
-
                             offset.x -= 25 + generator.finalRoomInfoArray[i].width * 6;
                             break;
                         case 2:
-                            GUI.Label(new Rect(offset.x, Screen.height - (offset.y), 5, 5),
+                            GUI.Label(new Rect(offset.x + (generator.finalRoomInfoArray[i].width / 2) * 5, Screen.height - (offset.y + generator.finalRoomInfoArray[i].height * 5), 5, 5),
                                 "", doorStyle);
-
                             offset.y += 25 + generator.finalRoomInfoArray[i].height * 6;
                             break;
                         case 3:
-                            GUI.Label(new Rect(offset.x, Screen.height - (offset.y), 5, 5),
+                            GUI.Label(new Rect(offset.x - 5 + generator.finalRoomInfoArray[i].width * 5, Screen.height - (offset.y + (generator.finalRoomInfoArray[i].height / 2) * 5), 5, 5),
                                 "", doorStyle);
-
                             offset.x += 25 + generator.finalRoomInfoArray[i].width * 6;
                             break;
                         default:
